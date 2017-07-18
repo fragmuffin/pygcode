90d979bb05a2e3ef66cf8acf3813d468df83c78c
Peter Boin <nymphii@gmail.com>
2017-07-18 15:05:15 +1000

    preparing for release

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pygcode',
+  packages = ['pygcode'],
+  version = '0.1',
+  description = 'basic g-code parser, interpreter, and writer library',
+  author = 'Peter Boin',
+  author_email = 'peter.boin@gmail.com',
+  url = 'https://github.com/fragmuffin/pygcode',
+  download_url = 'https://github.com/fragmuffin/pygcode/archive/0.1.tar.gz',
+  keywords = ['gcode', 'cnc', 'parser', 'interpreter'],
+  classifiers = [],
+)
