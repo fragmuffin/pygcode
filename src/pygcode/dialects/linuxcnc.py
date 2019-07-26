1729aeb9be89ce9f606f37d656dd76cce21e21fd
Walker Inman <winman@lucidsci.com>
2019-07-25 22:02:18 -0400

    don't remove decimal from floats

--- src/pygcode/dialects/linuxcnc.py
+++ src/pygcode/dialects/linuxcnc.py
@@ -30,7 +30,12 @@ def _clean_codestr(value):
     return "%g" % value
 
 CLEAN_NONE = lambda v: v
-CLEAN_FLOAT = lambda v: "{0:g}".format(round(v, 3))
+
+def CLEAN_FLOAT(v):
+    fstr = "{0:g}".format(round(v, 3))
+    if '.' not in fstr:
+        return fstr + '.'
+    return fstr
 CLEAN_CODE = _clean_codestr
 CLEAN_INT = lambda v: "%g" % v
 
